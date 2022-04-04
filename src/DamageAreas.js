f373a78b73f9f4c57c62c776cf5f0615139c2970
barisaskaleli <barisaskaleli@gmail.com>
2022-04-04 15:17:24 +0300

    Initial commit

---
+++ src/DamageAreas.js
@@ -0,0 +1,104 @@
+export const damageAreas = [
+  {
+    value: "LEFT_FRONT_FENDER",
+    text: "Left Front Fender"
+  },
+  {
+    value: "RIGHT_FRONT_FENDER",
+    text: "Right Front Fender"
+  },
+  {
+    value: "LEFT_FRONT_DOOR",
+    text: "Left Front Door"
+  },
+  {
+    value: "RIGHT_FRONT_DOOR",
+    text: "Right Front Door"
+  },
+  {
+    value: "LEFT_REAR_DOOR",
+    text: "Left Rear Door"
+  },
+  {
+    value: "RIGHT_REAR_DOOR",
+    text: "Right Rear Door"
+  },
+  {
+    value: "LEFT_REAR_FENDER",
+    text: "Left Rear Fender"
+  },
+  {
+    value: "RIGHT_REAR_FENDER",
+    text: "Right Fear Fender"
+  },
+  {
+    value: "FRONT_HOOD",
+    text: "Front Hood"
+  },
+  {
+    value: "REAR_HOOD",
+    text: "Rear Hood"
+  },
+  {
+    value: "CEILING",
+    text: "Ceiling"
+  }
+];
+
+export const damageSections = {
+  LEFT_FRONT_FENDER: {
+    state: "CLEAR",
+    sectionType: "LEFT_FRONT_FENDER",
+    color: "#f6f6f6"
+  },
+  RIGHT_FRONT_FENDER: {
+    state: "CLEAR",
+    sectionType: "RIGHT_FRONT_FENDER",
+    color: "#f6f6f6"
+  },
+  LEFT_FRONT_DOOR: {
+    state: "CLEAR",
+    sectionType: "LEFT_FRONT_DOOR",
+    color: "#f6f6f6"
+  },
+  RIGHT_FRONT_DOOR: {
+    state: "CLEAR",
+    sectionType: "RIGHT_FRONT_DOOR",
+    color: "#f6f6f6"
+  },
+  LEFT_REAR_DOOR: {
+    state: "CLEAR",
+    sectionType: "LEFT_REAR_DOOR",
+    color: "#f6f6f6"
+  },
+  RIGHT_REAR_DOOR: {
+    state: "CLEAR",
+    sectionType: "RIGHT_REAR_DOOR",
+    color: "#f6f6f6"
+  },
+  LEFT_REAR_FENDER: {
+    state: "CLEAR",
+    sectionType: "LEFT_REAR_FENDER",
+    color: "#f6f6f6"
+  },
+  RIGHT_REAR_FENDER: {
+    state: "CLEAR",
+    sectionType: "RIGHT_REAR_FENDER",
+    color: "#f6f6f6"
+  },
+  FRONT_HOOD: {
+    state: "CLEAR",
+    sectionType: "FRONT_HOOD",
+    color: "#f6f6f6"
+  },
+  REAR_HOOD: {
+    state: "CLEAR",
+    sectionType: "REAR_HOOD",
+    color: "#f6f6f6"
+  },
+  CEILING: {
+    state: "CLEAR",
+    sectionType: "CEILING",
+    color: "#f6f6f6"
+  }
+};
